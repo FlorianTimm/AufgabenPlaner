62925942714c2b35b998e15b30cf02526dc5ea9a
timmfl <timmfl@LGV-W-D645024.fhhnet.stadt.hamburg.de>
2017-11-07 12:33:38 +0100

    Korrektur

---
+++ src/de/florian_timm/aufgabenPlaner/AufgabenPlaner.java
@@ -0,0 +1,23 @@
+package de.florian_timm.aufgabenPlaner;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JFrame;
+
+public class AufgabenPlaner extends JFrame implements ActionListener {
+
+	/**
+	 *  GUI
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public static void main(String[] args) {
+		new AufgabenPlaner();
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+
+	}
+}
